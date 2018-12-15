ac78869ca32beb8c833f77e676d91636d5661e09
dtafesse <dtafesse@uwm.edu>
2018-12-15 11:11:02 -0600

    invoking cloud functions

--- firebase-auth/components/SignUp.js
+++ firebase-auth/components/SignUp.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { FormLabel, FormInput, Button } from "react-native-elements";
+import axios from "axios";
+
+const ROOT_URL =
+  "https://us-central1-one-time-password-b31ae.cloudfunctions.net";
 
 class SignUp extends Component {
   state = { phone: "" };
 
-  handleSubmit = () => {};
+  handleSubmit = () => {
+    axios
+      .post(`${ROOT_URL}/createUser`, {
+        phone: this.state.phone
+      })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+          phone: this.state.phone
+        });
+      });
+  };
 
   render() {
     return (
