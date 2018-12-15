c86770e7a284730cc20cfdfd281eaa3d970e2e44
dtafesse <dtafesse@uwm.edu>
2018-12-14 20:44:42 -0600

    creating new users - and testing

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -7,9 +7,11 @@ module.exports = (req, res) => {
   }
 
   // Format the phone number to remove dashes and parens, anything but digits
-  const phone = String(req.body.phone).replace(/[^\d]/g);
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
-  // Create a new user account using that phone number
-
-  // Respond to the user request, saying that account was made
+  admin
+    .auth()
+    .createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 };
