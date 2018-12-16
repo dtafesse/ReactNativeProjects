18a4515068196213f438881575c7c0592825bbaf
dtafesse <dtafesse@uwm.edu>
2018-12-15 19:25:11 -0600

    programmatic navigation

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
 
 class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { navigate } = navigation;
+    return {
+      headerTitle: "Review Jobs",
+      tabBarLabel: "Review Jobs",
+      headerRight: (
+        <Button title='Settings' onPress={() => navigate("settings")} />
+      )
+    };
+  };
+
   render() {
     return (
       <View>
