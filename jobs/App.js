d0afb9d1fe06c6cb5b241589beb37e309415508e
dtafesse <dtafesse@uwm.edu>
2018-12-15 18:35:55 -0600

    nesting navigators

--- jobs/App.js
+++ jobs/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
+
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import ReviewScreen from "./screens/ReviewScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+const MainNavigator = createBottomTabNavigator({
+  welcome: WelcomeScreen,
+  auth: AuthScreen,
+  main: createBottomTabNavigator({
+    map: MapScreen,
+    deck: DeckScreen,
+    review: createStackNavigator({
+      review: ReviewScreen,
+      settings: SettingsScreen
+    })
+  })
+});
+
+const AppContainer = createAppContainer(MainNavigator);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
