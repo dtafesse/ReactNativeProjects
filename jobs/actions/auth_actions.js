457557914e2df2f1d0e9186c4daad816d23f740e
dtafesse <dtafesse@uwm.edu>
2018-12-18 15:35:11 -0600

    testing facebook login

---
+++ jobs/actions/auth_actions.js
@@ -0,0 +1,35 @@
+import { AsyncStorage } from "react-native";
+import { Facebook } from "expo";
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from "./types";
+
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem("fb_token");
+  if (token) {
+    dispatch({
+      type: FACEBOOK_LOGIN_SUCCESS,
+      payload: token
+    });
+  } else {
+    // helper func for this action creator
+    doFaceBookLogin(dispatch);
+  }
+};
+
+const doFaceBookLogin = async dispatch => {
+  try {
+    let { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      "2206476529590136",
+      {
+        permissions: ["public_profile"]
+      }
+    );
+
+    if (type === "cancel") {
+      return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+    }
+    await AsyncStorage.setItem("fb_token", token);
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- jobs/screens/AuthScreen.js
+++ jobs/screens/AuthScreen.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class AuthScreen extends Component {
+  componentDidMount() {
+    this.props.facebookLogin();
+  }
+
   render() {
-    return (
-      <View>
-        <Text>AuthScreen</Text>
-      </View>
-    );
+    return <View />;
   }
 }
 
-export default AuthScreen;
+export default connect(
+  null,
+  actions
+)(AuthScreen);
