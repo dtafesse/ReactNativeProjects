c7110d58bdf8e39dba007b21c97b9e6eba54e542
dtafesse <dtafesse@uwm.edu>
2018-12-19 21:15:28 -0600

    rendering a list of liked jobs

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, Platform } from "react-native";
-import { Button } from "react-native-elements";
+import { View, Text, Platform, ScrollView, StyleSheet } from "react-native";
+import { Button, Card } from "react-native-elements";
 import { connect } from "react-redux";
 
 class ReviewScreen extends Component {
@@ -23,14 +23,36 @@ class ReviewScreen extends Component {
     };
   };
 
+  renderLikedJobs() {
+    return this.props.likedJobs.map(job => {
+      return (
+        <Card>
+          <View style={{ height: 200 }}>
+            <View style={styles.detailWrapper}>
+              <Text style={styles.italics}>{job.company}</Text>
+              <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+            </View>
+          </View>
+        </Card>
+      );
+    });
+  }
+
   render() {
-    return (
-      <View>
-        <Text>ReviewScreen</Text>
-      </View>
-    );
+    return <ScrollView>{this.renderLikedJobs()}</ScrollView>;
   }
 }
+
+const styles = StyleSheet.create({
+  detailWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10
+  },
+  italics: {
+    fontStyle: "italic"
+  }
+});
 
 function mapStateToProps(state) {
   return { likedJobs: state.likedJobs };
