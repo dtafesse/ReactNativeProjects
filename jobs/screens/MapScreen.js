83d784bda7322d9a560611c0860413ce748ec226
dtafesse <dtafesse@uwm.edu>
2018-12-19 19:13:27 -0600

    issuing action creators from MapScreen

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
 import { MapView } from "expo";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class MapScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -27,6 +30,10 @@ class MapScreen extends Component {
     this.setState({ region });
   };
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  };
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -42,9 +49,30 @@ class MapScreen extends Component {
           region={this.state.region}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title='Search This Area'
+            backgroundColor='#009688'
+            icon={{ name: "search" }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = StyleSheet.create({
+  buttonContainer: {
+    position: "absolute",
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+});
+
+export default connect(
+  null,
+  actions
+)(MapScreen);
