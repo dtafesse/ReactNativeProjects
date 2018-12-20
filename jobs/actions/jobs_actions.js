bfc7c14276a517e4e402412ce8bec3be82d6151b
dtafesse <dtafesse@uwm.edu>
2018-12-19 19:03:34 -0600

    fetching a list of jobs

--- jobs/actions/jobs_actions.js
+++ jobs/actions/jobs_actions.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import reverseGeocode from "latlng-to-zip";
 import qs from "qs";
 import { FETCH_JOBS } from "./types";
+import JOB_DATA from "./IndeedJobData.json";
+
+const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
 
 const JOB_QUERY_PARAMS = {
   publisher: "4201738803816157",
@@ -12,9 +15,18 @@ const JOB_QUERY_PARAMS = {
   q: "javascript"
 };
 
+const buildJobsUrl = zip => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = region => async dispatch => {
   try {
-    let zipCode = await reverseGeocode(region);
+    // let zipCode = await reverseGeocode(region);
+    // const url = buildJobsUrl(zip);
+    // let { data } = await axios.get(url);
+    const data = JOB_DATA;
+    dispatch({ type: FETCH_JOBS, payload: data });
   } catch (err) {
     console.error(err);
   }
