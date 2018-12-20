a6c3ca899c54f393ce55cefb244d2d9b4f16d81d
dtafesse <dtafesse@uwm.edu>
2018-12-19 18:29:45 -0600

    interacting with map view

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -9,10 +9,19 @@ class MapScreen extends Component {
     };
   };
 
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} />
+        <MapView style={{ flex: 1 }} region={this.state.region} />
       </View>
     );
   }
