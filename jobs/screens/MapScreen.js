67009925995701c34bc4c2419940f926cea99e41
dtafesse <dtafesse@uwm.edu>
2018-12-19 18:45:07 -0600

    onRegionChangeComplete callbacl

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -23,6 +23,10 @@ class MapScreen extends Component {
     this.setState({ mapLoaded: true });
   }
 
+  onRegionChangeComplete = region => {
+    this.setState({ region });
+  };
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -33,7 +37,11 @@ class MapScreen extends Component {
     }
     return (
       <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} region={this.state.region} />
+        <MapView
+          style={{ flex: 1 }}
+          region={this.state.region}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
       </View>
     );
   }
